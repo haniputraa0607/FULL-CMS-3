f74eaeba23af9ed009d051b0dadf4d4c5afbe653
Tri Wardana <tri.wardana@technopartner.id>
2019-02-27 09:21:11 +0700

    news with available image

--- Modules/News/Resources/views/webview/news.blade.php
+++ Modules/News/Resources/views/webview/news.blade.php
@@ -18,7 +18,7 @@
         }
 
         .kotak-title {
-        	padding-top: 10px;
+            padding-top: 10px;
             padding-left: 41.3px;
             padding-right: 41.3px;
             background: #fff;
@@ -26,13 +26,13 @@
             height: 100%
         }
 
-    	.kotak2 {
-    		padding-top: 10px;
-    		padding-bottom: 10px;
-    		padding-left: 26.3px;
-    		padding-right: 26.3px;
-    		background: #fff;
-    		font-family: 'Open Sans', sans-serif;
+        .kotak2 {
+            padding-top: 10px;
+            padding-bottom: 10px;
+            padding-left: 26.3px;
+            padding-right: 26.3px;
+            background: #fff;
+            font-family: 'Open Sans', sans-serif;
             height: 100%
         }
 
@@ -55,63 +55,63 @@
             left: calc(50% - 80px);
         }
 
-    	.completed {
-    		color: green;
-    	}
+        .completed {
+            color: green;
+        }
 
-    	.bold {
-    		font-weight: bold;
-    	}
+        .bold {
+            font-weight: bold;
+        }
 
         .space-bottom {
             padding-bottom: 8px;
         }
 
         .space-title {
-        	padding-bottom: 1px;
+            padding-bottom: 1px;
         }
 
         .space-top {
             padding-top: 15px;
         }
 
-    	.space-text {
-    		padding-bottom: 10px;
-    	}
+        .space-text {
+            padding-bottom: 10px;
+        }
 
-    	.line-bottom {
-    		    border-bottom: 1px solid #eee;
+        .line-bottom {
+                border-bottom: 1px solid #eee;
                 margin-bottom: -10px;
                 margin-top: 8px;
-    	}
+        }
 
-    	.text-grey {
-    		color: #aaaaaa;
-    	}
+        .text-grey {
+            color: #aaaaaa;
+        }
 
-    	.text-much-grey {
-    		color: #bfbfbf;
-    	}
+        .text-much-grey {
+            color: #bfbfbf;
+        }
 
-    	.text-black {
-    		color: #000000;
-    	}
+        .text-black {
+            color: #000000;
+        }
 
-    	.text-medium-grey {
-    		color: #806e6e6e;
-    	}
+        .text-medium-grey {
+            color: #806e6e6e;
+        }
 
-    	.text-grey-white {
-    		color: #666;
-    	}
+        .text-grey-white {
+            color: #666;
+        }
 
-    	.text-grey-black {
-    		color: #4c4c4c;
-    	}
+        .text-grey-black {
+            color: #4c4c4c;
+        }
 
-    	.text-grey-red {
-    		color: #9a0404;
-    	}
+        .text-grey-red {
+            color: #9a0404;
+        }
 
         .text-grey-red-cancel {
             color: rgba(154,4,4,1);
@@ -125,44 +125,44 @@
             color: rgba(227,159,0,1);
         }
 
-    	.text-grey-green {
-    		color: rgba(4,154,74,1);
-    	}
+        .text-grey-green {
+            color: rgba(4,154,74,1);
+        }
 
-    	.open-sans-font {
-    		font-family: 'Open Sans', sans-serif;
-    	}
+        .open-sans-font {
+            font-family: 'Open Sans', sans-serif;
+        }
 
-    	.questrial-font {
-    		font-family: 'Questrial', sans-serif;
-    	}
+        .questrial-font {
+            font-family: 'Questrial', sans-serif;
+        }
 
-    	.text-14-3px {
-    		font-size: 14.3px;
-    	}
+        .text-14-3px {
+            font-size: 14.3px;
+        }
 
-    	.text-14px {
-    		font-size: 14px;
-    	}
+        .text-14px {
+            font-size: 14px;
+        }
 
         .text-15px {
             font-size: 15px;
         }
 
-    	.text-13-3px {
-    		font-size: 13.3px;
-    	}
+        .text-13-3px {
+            font-size: 13.3px;
+        }
 
-    	.text-12-7px {
-    		font-size: 12.7px;
-    	}
+        .text-12-7px {
+            font-size: 12.7px;
+        }
 
-    	.text-12px {
-    		font-size: 12px;
-    	}
+        .text-12px {
+            font-size: 12px;
+        }
 
         .text-11-7px {
-        	font-size: 11.7px;
+            font-size: 11.7px;
         }
 
         .text-custom-px {
@@ -171,7 +171,7 @@
         }
 
         .logo-img {
-        	width: 14.7px;
+            width: 14.7px;
             height: 14.7px;
             margin-top: -7px;
             margin-right: 5px;
@@ -235,11 +235,11 @@ background: -webkit-gradient(left top, left bottom, color-stop(50%, rgba(255,255
 background: -webkit-linear-gradient(top, rgba(255,255,255,0) 50%, rgba(255,255,255,1) 82%);
 background: -o-linear-gradient(top, rgba(255,255,255,0) 50%, rgba(255,255,255,1) 82%);
 background: -ms-linear-gradient(top, rgba(255,255,255,0) 50%, rgba(255,255,255,1) 82%);
-background: linear-gradient(to bottom, rgba(255,255,255,0) 50%, rgba(255,255,255,1) 82%);
+background: linear-gradient(to bottom, rgba(255,255,255,0) 60%, rgba(255,255,255,1) 82%);
 filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', endColorstr='#ffffff', GradientType=0 );
     height: 100px;
     width: 100%;
-    margin-top: -78px;
+    margin-top: -81px;
         }
 
         </style>
@@ -248,6 +248,9 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', end
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     </head>
     <body>
+        @php
+           // print_r($news[0]['news_outlet']);die();
+        @endphp
 
         <div id="box" class="box-shadow"></div>
         <div class="kotak">
@@ -264,49 +267,70 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', end
         </div>
 
         @if (isset($news[0]['news_event_date_start']) || isset($news[0]['news_event_location_name']) || isset($news[0]['news_event_location_address']) || isset($news[0]['news_event_location_phone']) || isset($news[0]['news_event_location_map']))
-      	<div class="kotak1">
-      		<div class="container">
+        <div class="kotak1">
+            <div class="container">
             @if (isset($news[0]['news_event_date_start']))
-      	   		<div class="row space-bottom">
-      	   			<div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/calendar.png') }}"> <span> {{ date('d F', strtotime($news[0]['news_event_date_start'])) }} - {{ date('d F Y', strtotime($news[0]['news_event_date_end'])) }} </span></div>
-      	   		</div>
+                @if ($news[0]['news_event_date_start'] == $news[0]['news_event_date_end'])
+                    <div class="row space-bottom">
+                        <div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/calendar.png') }}"> 
+                        <span> {{ date('d F Y', strtotime($news[0]['news_event_date_start'])) }} </span></div>
+                    </div>
+                @else
+                    <div class="row space-bottom">
+                        <div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/calendar.png') }}"> 
+                        <span> {{ date('d F', strtotime($news[0]['news_event_date_start'])) }} - {{ date('d F Y', strtotime($news[0]['news_event_date_end'])) }} </span></div>
+                    </div>
+                @endif
+            @endif
+            @if (isset($news[0]['news_event_time_start']))
+                @if ($news[0]['news_event_time_start'] == $news[0]['news_event_time_end'])
+                    <div class="row space-bottom">
+                        <div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/time.png') }}"> 
+                        <span> {{ date('H:i', strtotime($news[0]['news_event_time_start'])) }}</span></div>
+                    </div>
+                @else
+                    <div class="row space-bottom">
+                        <div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/time.png') }}"> 
+                        <span>{{ date('H:i', strtotime($news[0]['news_event_time_start'])) }} - {{ date('H:i', strtotime($news[0]['news_event_time_end'])) }}</span></div>
+                    </div>
+                @endif
             @endif
             @if (isset($news[0]['news_event_location_name']))
-      	   		<div class="row space-bottom">
-      	   			<div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/outlet.png') }}"> <span> {{ $news[0]['news_event_location_name'] }} </span></div>
-      	   		</div>
+                <div class="row space-bottom">
+                    <div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/outlet.png') }}"> <span> {{ $news[0]['news_event_location_name'] }} </span></div>
+                </div>
             @endif
 
             @if (isset($news[0]['news_event_location_address']))
-      	   		<div class="row space-bottom">
-      	   			<div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/marker.png') }}"> <span>{{ $news[0]['news_event_location_address'] }} </span></div>
-      	   		</div>
+                <div class="row space-bottom">
+                    <div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/marker.png') }}"> <span>{{ $news[0]['news_event_location_address'] }} </span></div>
+                </div>
             @endif
 
             @if (isset($news[0]['news_event_location_phone']))
-      	   		<div class="row space-bottom">
-      	   			<div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/marker.png') }}"> <span> {{ $news[0]['news_event_location_phone'] }} </span></div>
-      	   		</div>
+                <div class="row space-bottom">
+                    <div class="col-12 text-grey-black text-12px open-sans-font"><img class="logo-img" src="{{ url('img/webview/hp.png') }}"> <span> {{ $news[0]['news_event_location_phone'] }} </span></div>
+                </div>
             @endif
 
             @if (isset($news[0]['news_event_location_map']))
-      	   		<div class="row space-bottom">
-      	   			<div class="col-12 text-grey-black text-12px open-sans-font"><div id="map-canvas" style="width:500;height:180px;"></div></div>
-      	   		</div>
+                <div class="row space-bottom">
+                    <div class="col-12 text-grey-black text-12px open-sans-font"><div id="map-canvas" style="width:500;height:180px;"></div></div>
+                </div>
             @endif
 
-    	   	</div>
+            </div>
             <div class="line-bottom">
                 </div>
-      	</div>
+        </div>
         @endif
 
-      	<div class="kotak2">
-      		<div class="container">
-    	   		<div class="row space-bottom text-12px">{!! $news[0]['news_content_long'] !!}
-    	   		</div>
-    	   	</div>
-      	</div>
+        <div class="kotak2">
+            <div class="container">
+                <div class="row space-bottom text-12px">{!! $news[0]['news_content_long'] !!}
+                </div>
+            </div>
+        </div>
 
         @if (isset($news[0]['news_video_text']))
             <div class="kotak2">
@@ -323,6 +347,8 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', end
                                 <iframe id="frame1" class="embed-responsive-item" src="https://www.youtube.com/embed/{{ substr($news[0]['news_video'], strpos($news[0]['news_video'], "=") + 1) }}?rel=0" allowfullscreen></iframe><div class="questrial-font image-caption-you text-13-3px"></div>
                             </div>
                         </div>
+                    @else
+                        <img style="height: 60vw;object-fit: cover" src="https://via.placeholder.com/800x500.png?text=No Video Available">
                     @endif
                 </div>
             </div>
@@ -342,7 +368,15 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', end
                     <div class="row">
                         <div class="owl-carousel owl-theme">
                             @foreach ($news[0]['news_outlet'] as $key => $value)
-                                <div class="item"> <img style="height: 60vw;" src="{{ $value['outlet']['photos'][0]['url_outlet_photo'] }}"> <div class="questrial-font image-caption-all text-13-3px">{{ $value['outlet']['outlet_name'] }}</div> </div>
+                                <div class="item">
+                                    @if (isset($value['outlet']['photos'][0]['url_outlet_photo']))
+                                        <img style="height: 60vw;object-fit: cover" src="{{ $value['outlet']['photos'][0]['url_outlet_photo'] }}"> 
+                                        <div class="questrial-font image-caption-all text-13-3px">{{ $value['outlet']['outlet_name'] }}</div>
+                                    @else
+                                        <img style="height: 60vw;object-fit: cover" src="https://via.placeholder.com/800x500.png?text=No Image Available"> 
+                                        <div class="questrial-font image-caption-all text-13-3px">{{ $value['outlet']['outlet_name'] }}</div>
+                                    @endif
+                                </div>
 
                             @endforeach
                         </div>
@@ -362,12 +396,15 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', end
 
                         </div>
                     @endif
-
                     @if (isset($news[0]['news_product']))
                         <div class="row space-bottom">
                             <div class="owl-carousel owl-theme">
                                 @foreach ($news[0]['news_product'] as $key => $value)
-                                    <div class="item"> <img style="height: 60vw !important;" src="{{ $value['product']['photos'][0]['url_product_photo'] }}"> <div class="questrial-font image-caption-all text-13-3px">{{ $value['product']['product_name'] }}</div> </div>
+                                    @if (isset($value['product']['photos'][0]['url_product_photo']))
+                                        <div class="item"> <img style="height: 60vw !important; object-fit: cover" src="{{ $value['product']['photos'][0]['url_product_photo'] }}"> <div class="questrial-font image-caption-all text-13-3px">{{ $value['product']['product_name'] }}</div> </div>
+                                    @else
+                                        <div class="item"> <img style="height: 60vw !important; object-fit: cover" src="https://via.placeholder.com/800x500.png?text=No Image Available""> <div class="questrial-font image-caption-all text-13-3px">{{ $value['product']['product_name'] }}</div> </div>
+                                    @endif
                                 @endforeach
                             </div>
                         </div>
@@ -396,7 +433,7 @@ filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffffff', end
             $(".owl-carousel").owlCarousel({
                 margin: 1,
                 items : 1,
-                loop:true,
+                loop:false,
             });
 
             owl = $('.owl-carousel').owlCarousel();
