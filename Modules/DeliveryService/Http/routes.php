39794eb63b7bab33043e6f066f85177b2a52e811
Yogie Setya Nugraha <yogie@technopartner.id>
2019-09-24 15:43:55 +0700

    Add webview route

--- Modules/DeliveryService/Http/routes.php
+++ Modules/DeliveryService/Http/routes.php
@@ -1,6 +1,9 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'deliveryservice', 'namespace' => 'Modules\DeliveryService\Http\Controllers'], function()
-{
+Route::group(['middleware' => 'web', 'prefix' => 'delivery-service', 'namespace' => 'Modules\DeliveryService\Http\Controllers'], function () {
     Route::get('/', 'DeliveryServiceController@index');
+});
+
+Route::group(['prefix' => 'delivery-service', 'namespace' => 'Modules\DeliveryService\Http\Controllers'], function () {
+    Route::get('webview', 'DeliveryServiceController@detailWebview');
 });
