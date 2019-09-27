d56379a6faa538829abc35c1662921d6ed4469d9
tpid-yogie <yogie@technopartner.id>
2019-09-27 13:41:04 +0700

    Create, Read, Update Brand Route

--- Modules/Brand/Http/routes.php
+++ Modules/Brand/Http/routes.php
@@ -1,6 +1,8 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'brand', 'namespace' => 'Modules\Brand\Http\Controllers'], function()
-{
+Route::group(['middleware' => 'web', 'prefix' => 'brand', 'namespace' => 'Modules\Brand\Http\Controllers'], function () {
     Route::get('/', 'BrandController@index');
+    Route::get('create', 'BrandController@create');
+    Route::get('show/{id}', 'BrandController@show');
+    Route::post('store', 'BrandController@store');
 });
