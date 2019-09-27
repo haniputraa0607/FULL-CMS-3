a518801788838ba2e24e51e5e25b03a0a5b39e88
tpid-yogie <yogie@technopartner.id>
2019-09-27 09:00:38 +0700

    Add brand sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -228,6 +228,32 @@
 				@endif
 			@endif
 
+			@if(MyHelper::hasAccess([155,156,157,158,159], $grantedFeature))
+			<li class="nav-item {{($menu_active == 'faq') ? 'active' : ''}}">
+				<a href="javascript:;" class="nav-link nav-toggle">
+					<i class="icon-question"></i>
+					<span class="title">Brand</span>
+					<span class="arrow {{($menu_active == 'faq') ? 'open' : ''}}"></span>
+				</a>
+				<ul class="sub-menu">
+					@if(MyHelper::hasAccess([156], $grantedFeature))
+					<li class="nav-item {{($submenu_active == 'faq-new') ? 'active open' : ''}}">
+						<a href="{{url('setting/faq/create')}}" class="nav-link ">
+							<span class="title">New Brand</span>
+						</a>
+					</li>
+					@endif
+					@if(MyHelper::hasAccess([155,157,158,159], $grantedFeature))
+					<li class="nav-item {{($submenu_active == 'faq-list') ? 'active open' : ''}}">
+						<a href="{{url('setting/faq')}}" class="nav-link ">
+							<span class="title">List Brand</span>
+						</a>
+					</li>
+					@endif
+				</ul>
+			</li>
+			@endif
+
 			@if(MyHelper::hasAccess([24,26,32,33,34], $grantedFeature))
 			<li class="nav-item {{($menu_active == 'outlet') ? 'active' : ''}}">
 				<a href="javascript:;" class="nav-link nav-toggle">
