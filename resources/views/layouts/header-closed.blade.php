19a8d4431a8aaad1197072ac2d49460c15f5ce39
tpid-hani <haniyah.imtiyaz@technopartner.id>
2019-10-14 13:31:09 +0700

    update style height & margin in logo header closed

--- resources/views/layouts/header-closed.blade.php
+++ resources/views/layouts/header-closed.blade.php
@@ -4,7 +4,7 @@
 		<!-- BEGIN LOGO -->
 		<div class="page-logo">
 			<a href="index.html">
-				<img src="{{ env('AWS_ASSET_URL') }}{{ ('images/logo.png') }}" alt="logo" class="logo-default"  style="margin: 8px 0 0;"/> </a>
+				<img src="{{ env('AWS_ASSET_URL') }}{{ ('images/logo.png') }}" alt="logo" class="logo-default"  style="margin: 0; height:45px"/> </a>
 			<div class="menu-toggler sidebar-toggler">
 				<span></span>
 			</div>
