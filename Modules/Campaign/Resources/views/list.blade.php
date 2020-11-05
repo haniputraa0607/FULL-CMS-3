85c3e389d1206666dee37934099f6854ba652050
Dina <dina@technopartner.id>
2020-11-05 10:17:44 +0700

    update pagination campaign list

--- Modules/Campaign/Resources/views/list.blade.php
+++ Modules/Campaign/Resources/views/list.blade.php
@@ -118,8 +118,8 @@ $grantedFeature     = session('granted_features');
 			<div class="portlet-body">
 				<div class="col-md-12" style="padding-left:0px;padding-right:0px;margin-top:20px;margin-bottom:15px">
 					<div class="col-md-6" style="padding: 0px;">
-						<form action="" method="POST">
-						<input type="text" class="form-control" name="campaign_title" placeholder="Search Campaign Title" @if(isset($post['campaign_title']) && $post['campaign_title'] != "") value = "{{$post['campaign_title']}}" @endif>
+						<form action="?filter=1" method="POST">
+							<input type="text" class="form-control" name="campaign_title" placeholder="Search Campaign Title" @if(isset($post['campaign_title']) && $post['campaign_title'] != "") value = "{{$post['campaign_title']}}" @endif>
 					</div>
 					<div class="col-md-1" style="padding: 0px;">
 						{{ csrf_field() }}
@@ -129,92 +129,77 @@ $grantedFeature     = session('granted_features');
 					<div class="col-md-1" style="padding: 0px;">
 						@if(isset($post['campaign_title']) && $post['campaign_title'] != "")<a href="{{url('campaign')}}" class="btn red">Reset Search</a>@endif
 					</div>
-					<div class="col-md-4" style="padding: 0px;">
-						<div class="pull-right pagination" style="margin-top: 0px;margin-bottom: 0px;">
-							<ul class="pagination" style="margin-top: 0px;margin-bottom: 0px;">
-							@if(isset($post['campaign_title']) && $post['campaign_title'] != "")
-							@else
-								@if($post['skip'] > 0)
-									<li class="page-first"><a href="{{url('campaign')}}/page/{{(($post['skip'] + $post['take'])/$post['take'])-1}}">«</a></li>
-								@else
-									<li class="page-first disabled"><a href="javascript:void(0)">«</a></li>
-								@endif
-								
-								@if(isset($count) && $count > (($post['skip']+1) * $post['take']))
-									<li class="page-last"><a href="{{url('campaign')}}/page/{{(($post['skip'] + $post['take'])/$post['take'])+1}}">»</a></li>
-								@else
-									<li class="page-last disabled"><a href="javascript:void(0)">»</a></li>
-								@endif
-							
-							@endif
-							</ul>
-						</div>
-					</div>
 				</div>
 				<div class="table-scrollable">
-					@if(isset($result) && $result != '')
 					<table class="table table-striped table-bordered table-hover" id="sample_1">
 						<thead>
-							<tr>
-								<th>No</th>
-								<th>Title</th>
-								<th>Send At</th>
-								<th>Media</th>
-								<th width=10%>Action</th>
-							</tr>
+						<tr>
+							<th>No</th>
+							<th>Title</th>
+							<th>Send At</th>
+							<th>Media (All | Sent)</th>
+							<th width=10%>Action</th>
+						</tr>
 						</thead>
 						<tbody>
-							@foreach($result as $key => $data)
-							<tr>
-								<td>{{(($post['skip'] + $post['take']) - $post['take'])+($key+1)}}</td>
-								<td>{{$data['campaign_title']}}</td>
-								<td>@if($data['campaign_send_at'] != "")
-										{{date('d F Y - H:i', strtotime($data['campaign_send_at']))}}
-									@else
-										{{date('d F Y - H:i', strtotime($data['created_at']))}}
-									@endif</td>
-								<td>
-									<ul>
-										@if($data['campaign_media_email'] == 'Yes')<li> Email ({{$data['campaign_email_count_all']}} | {{$data['campaign_email_count_queue']}} | {{$data['campaign_email_count_sent']}}) </li> @endif
-										@if($data['campaign_media_sms'] == 'Yes')<li> SMS ({{$data['campaign_sms_count_all']}} | {{$data['campaign_sms_count_queue']}} | {{$data['campaign_sms_count_sent']}})</li> @endif
-										@if($data['campaign_media_push'] == 'Yes')<li> Push ({{$data['campaign_push_count_all']}} | {{$data['campaign_push_count_queue']}} | {{$data['campaign_push_count_sent']}})</li> @endif
-										@if($data['campaign_media_inbox'] == 'Yes')<li> Inbox ({{$data['campaign_inbox_count']}})</li> @endif
-										@if($data['campaign_media_whatsapp'] == 'Yes')<li> WhatsApp ({{$data['campaign_whatsapp_count_all']}})</li> @endif
-									</ul>
-								</td>
-								<td>
-									<div class="btn-group pull-right">
-										<button class="btn blue btn-xs btn-outline dropdown-toggle" data-toggle="dropdown">Actions
-										<i class="fa fa-angle-down"></i>
-										</button> 
-										<ul class="dropdown-menu pull-right">
-											<li>
-												<a href="{{ url('campaign/step1') }}/{{ $data['id_campaign'] }}">
-												<i class="fa fa-edit"></i> Edit Information </a>
-											</li>
-											<li>
-												<a href="{{ url('campaign/step2') }}/{{ $data['id_campaign'] }}">
-												<i class="fa fa-edit"></i> Edit Receipient </a>
-											</li>
-											<li>
-												<a href="{{ url('campaign/step3') }}/{{ $data['id_campaign'] }}">
-												<i class="fa fa-gear"></i> Summary </a>
-											</li>
-											@if($data['campaign_is_sent'] != 'Yes' && MyHelper::hasAccess([102], $grantedFeature))
-												<li>
-													<a class="sweetalert-delete" data-id="{{ $data['id_campaign']}}" data-name="{{ $data['campaign_title']}}">
-														<i class="fa fa-trash"></i> Delete </a>
-												</li>
-											@endif
+						@if(!empty($campaign))
+							@foreach($campaign as $key => $data)
+								<tr>
+									<td>{{($post['page'] - 1) * 15 + $key + 1}}</td>
+									<td>{{$data['campaign_title']}}</td>
+									<td>@if($data['campaign_send_at'] != "")
+											{{date('d F Y - H:i', strtotime($data['campaign_send_at']))}}
+										@else
+											{{date('d F Y - H:i', strtotime($data['created_at']))}}
+										@endif</td>
+									<td>
+										<ul>
+											@if($data['campaign_media_email'] == 'Yes')<li> Email ({{$data['campaign_email_count_all']}} | {{$data['campaign_email_count_sent']}}) </li> @endif
+											@if($data['campaign_media_sms'] == 'Yes')<li> SMS ({{$data['campaign_sms_count_all']}} | {{$data['campaign_sms_count_sent']}})</li> @endif
+											@if($data['campaign_media_push'] == 'Yes')<li> Push ({{$data['campaign_push_count_all']}} | {{$data['campaign_push_count_sent']}})</li> @endif
+											@if($data['campaign_media_inbox'] == 'Yes')<li> Inbox ({{$data['campaign_inbox_count']}})</li> @endif
+											@if($data['campaign_media_whatsapp'] == 'Yes')<li> WhatsApp ({{$data['campaign_whatsapp_count_all']}})</li> @endif
 										</ul>
-									</div>
-								</td>
-							</tr>
+									</td>
+									<td>
+										<div class="btn-group pull-right">
+											<button class="btn blue btn-xs btn-outline dropdown-toggle" data-toggle="dropdown">Actions
+												<i class="fa fa-angle-down"></i>
+											</button>
+											<ul class="dropdown-menu pull-right">
+												@if($data['campaign_is_sent'] != 'Yes')
+													<li>
+														<a href="{{ url('campaign/step1') }}/{{ $data['id_campaign'] }}">
+															<i class="fa fa-edit"></i> Edit Information </a>
+													</li>
+													<li>
+														<a href="{{ url('campaign/step2') }}/{{ $data['id_campaign'] }}">
+															<i class="fa fa-edit"></i> Edit Receipient </a>
+													</li>
+													<li>
+														<a class="sweetalert-delete" data-id="{{ $data['id_campaign']}}" data-name="{{ $data['campaign_title']}}">
+															<i class="fa fa-trash"></i> Delete </a>
+													</li>
+												@endif
+												<li>
+													<a href="{{ url('campaign/step3') }}/{{ $data['id_campaign'] }}">
+														<i class="fa fa-gear"></i> Summary </a>
+												</li>
+											</ul>
+										</div>
+									</td>
+								</tr>
 							@endforeach
+						@else
+							<tr style="text-align: center"><td colspan="10">No Data Available</td></tr>
+						@endif
 						</tbody>
 					</table>
-					@else
-						No User found with such conditions
+				</div>
+				<br>
+				<div style="text-align: right">
+					@if ($campaignPaginator)
+						{{ $campaignPaginator->links() }}
 					@endif
 				</div>
 			</div>
