76e65b2916b449fb68df67a35083ee93b68099a8
damar <damar@technopartner.co.id>
2020-12-01 16:59:24 +0700

    add pagination to deals list

--- Modules/Deals/Resources/views/deals/list.blade.php
+++ Modules/Deals/Resources/views/deals/list.blade.php
@@ -17,6 +17,14 @@ $grantedFeature     = session('granted_features');
             vertical-align: middle!important;
             text-align: center;
         }
+        .middle-left {
+            vertical-align: middle!important;
+            text-align: left;
+        }
+        .paginator-right {
+            display: flex;
+            justify-content: flex-end;
+        }
     </style>
 @endsection
 
@@ -94,7 +102,7 @@ $grantedFeature     = session('granted_features');
                     [5, 10, 15, 20, "All"]
                 ],
                 pageLength: 10,
-                dom: "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>"
+                dom: "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'>>"
         });
 
         $('#sample_1').on('click', '.delete', function() {
@@ -262,6 +270,11 @@ $grantedFeature     = session('granted_features');
                     @endif
                 </tbody>
             </table>
+            <div class="paginator-right">
+                @if ($dealsPaginator)
+                  {{ $dealsPaginator->links() }}
+                @endif
+            </div>
         </div>
     </div>
 
