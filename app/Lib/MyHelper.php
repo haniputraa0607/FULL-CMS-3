931bb2fb84494fcb0963d45cb0f4b33647c052e4
Toto Prasetyo <totop275@gmail.com>
2021-03-22 12:00:33 +0700

    fix error export

--- app/Lib/MyHelper.php
+++ app/Lib/MyHelper.php
@@ -744,14 +744,7 @@ class MyHelper
    * @return String Excel column name (ex. A)
    */
   public static function getNameFromNumber($num) {
-      $numeric = ($num - 1) % 26;
-      $letter = chr(65 + $numeric);
-      $num2 = intval($num / 26);
-      if ($num2 > 0) {
-          return getNameFromNumber($num2 - 1) . $letter;
-      } else {
-          return $letter;
-      }
+      return \PhpOffice\PhpSpreadsheet\Cell\Coordinate::stringFromColumnIndex($num);
   }
 }
 
