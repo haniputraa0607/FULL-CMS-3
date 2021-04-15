843758d51694a95bc7eaa6fe0ec4359d46991ec9
tpid-damar <damar@technopartner.co.id>
2021-04-15 13:05:11 +0700

    [Added] image preview promo description

--- Modules/PromoCampaign/Resources/views/template/promo-description.blade.php
+++ Modules/PromoCampaign/Resources/views/template/promo-description.blade.php
@@ -27,6 +27,16 @@ $grantedFeature     = session('granted_features');
 				break;
 		}
 	@endphp
+	<style type="text/css">
+		.modal-dialog{
+		    position: relative;
+		    display: table; /* This is important */ 
+		    overflow-y: auto;    
+		    overflow-x: auto;
+		    width: auto;
+		    min-width: 300px;
+		}
+	</style>
 
 	<div class="portlet-body form">
 	    <div class="portlet light bordered">
@@ -36,38 +46,65 @@ $grantedFeature     = session('granted_features');
 	            </div>
 	        </div>
 		    <div class="portlet-body form">
-				<div class="">
-					<div class="profile-info">
-						<div class="row static-info">
-				            <div class="col-md-12 name"> Default Description :</div>
-				            <div class="col-md-6">
-				            	<blockquote style="font-size: 14px;margin-top: 12px"> {{ ($data_promo['description'] ?? '') }} </blockquote>
-				            </div>
-				        </div>
-					    <div class="row static-info">
-				            <div class="col-md-6 name"> Custom Description :</div>
-				            <div class="col-md-6"></div>
-				        </div>
-						<form id="form-promo-description" class="form-horizontal" role="form" action="{{ url('promo-campaign/promo-description') }}" method="post" enctype="multipart/form-data">
-							{{ csrf_field() }}
-					        <div class="row static-info">
-					            <div class="col-md-6 value">
-					            	<div class="input-icon right">
-										<textarea name="promo_description" id="field_content_long" class="form-control" placeholder="Deals Description" style="width: 490px;
-			  height: 150px;">{{ old('promo_description')??$data_promo['promo_description']??'' }}</textarea>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="profile-info">
+							<div class="row static-info">
+					            <div class="col-md-12 name"> Default Description :</div>
+					            <div class="col-md-12">
+					            	<blockquote style="font-size: 14px;margin-top: 12px"> {{ ($data_promo['description'] ?? '') }} </blockquote>
+					            </div>
+					        </div>
+						    <div class="row static-info">
+					            <div class="col-md-6 name"> Custom Description :</div>
+					            <div class="col-md-6"></div>
+					        </div>
+							<form id="form-promo-description" class="form-horizontal" role="form" action="{{ url('promo-campaign/promo-description') }}" method="post" enctype="multipart/form-data">
+								{{ csrf_field() }}
+						        <div class="row static-info">
+						            <div class="col-md-6 value">
+						            	<div class="input-icon right">
+											<textarea name="promo_description" id="field_content_long" class="form-control" placeholder="Deals Description" style="width: 490px;
+				  height: 150px;">{{ old('promo_description')??$data_promo['promo_description']??'' }}</textarea>
+										</div>
+										<input type="hidden" value="{{ $data_promo['id_deals'] ?? null }}" name="id_deals" />
+					                    <input type="hidden" value="{{ $data_promo['id_promo_campaign'] ?? null }}" name="id_promo_campaign" />
+					                    <input type="hidden" value="{{ $data_promo['id_subscription'] ?? null }}" name="id_subscription" />
+					                    <input type="hidden" value="{{ $data_promo['id_deals_promotion_template'] ?? null }}" name="id_deals_promotion_template" />
 									</div>
-									<input type="hidden" value="{{ $data_promo['id_deals'] ?? null }}" name="id_deals" />
-				                    <input type="hidden" value="{{ $data_promo['id_promo_campaign'] ?? null }}" name="id_promo_campaign" />
-				                    <input type="hidden" value="{{ $data_promo['id_subscription'] ?? null }}" name="id_subscription" />
-				                    <input type="hidden" value="{{ $data_promo['id_deals_promotion_template'] ?? null }}" name="id_deals_promotion_template" />
 								</div>
+				                <button type="submit" class="btn green">update</button>
+							</form>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<a href="javascript:;" class="img-preview" data-src="{{ env('STORAGE_URL_VIEW') }}img/deals/promo_description.jpg">
+							<div style="text-align: center;">
+								<img id="imageresource" class="zoom-in img-tutorial" src="{{ env('STORAGE_URL_VIEW') }}img/deals/promo_description.jpg" height="550px" />
+								<p style="text-align: center">(b)</p>
 							</div>
-			                <button type="submit" class="btn green">update</button>
-						</form>
+						</a>
 					</div>
 				</div>
 			</div>
         </div>
+	</div>
+
+	<div class="modal fade" id="image-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title" id="myModalLabel">Image preview</h4>
+				</div>
+				<div class="modal-body">
+					<img src="" id="image-preview">
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
 	</div>
 
 @endsection
@@ -88,4 +125,10 @@ $grantedFeature     = session('granted_features');
             }
 		});
 	</script> --}}
+	<script type="text/javascript">
+		$(".img-preview").on("click", function() {
+		   $('#image-preview').attr('src', $(this).data('src')); 
+		   $('#image-modal').modal('show');
+		});
+	</script>
 @endsection
