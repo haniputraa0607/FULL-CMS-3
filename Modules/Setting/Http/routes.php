ca0ea06920ee4ae05095aa06b03f254841b2b47a
Toto Prasetyo <toto@technopartner.co.id>
2022-10-25 10:25:48 +0700

    faq doctor

--- Modules/Setting/Http/routes.php
+++ Modules/Setting/Http/routes.php
@@ -13,7 +13,15 @@ Route::group(['middleware' => ['web', 'validate_session'], 'prefix' => 'setting'
     Route::post('faq/update/{slug}', 'SettingController@faqUpdate');
     Route::any('faq/delete/{slug}', 'SettingController@faqDelete');
     Route::get('faq/sort', 'SettingController@faqSort');
-    Route::post('faq/sort/update', 'SettingController@faqSortUpdate');
+    Route::post('faq/sort/update', 'SettingController@faqDoctorSortUpdate');
+    Route::get('faq-doctor', 'SettingController@faqDoctorList');
+    Route::get('faq-doctor/create', 'SettingController@faqDoctorCreate');
+    Route::post('faq-doctor/save', 'SettingController@faqDoctorStore');
+    Route::get('faq-doctor/edit/{slug}', 'SettingController@faqDoctorEdit');
+    Route::post('faq-doctor/update/{slug}', 'SettingController@faqDoctorUpdate');
+    Route::any('faq-doctor/delete/{slug}', 'SettingController@faqDoctorDelete');
+    Route::get('faq-doctor/sort', 'SettingController@faqDoctorSort');
+    Route::post('faq-doctor/sort/update', 'SettingController@faqDoctorSortUpdate');
     Route::post('update/{slug}', 'SettingController@settingUpdate');
 
     
